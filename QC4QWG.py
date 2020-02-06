aae3df50ccc5d80030b619aa56e23d81effc377a
西尾 真 <parton@sfc.wide.ad.jp>
2020-02-06 14:28:39 +0900

    added shintyoku

--- QC4QWG.py
+++ QC4QWG.py
@@ -19,11 +19,14 @@ def QC4QWG(G, step):
     from qiskit import QuantumCircuit
     from qiskit import QuantumRegister
     from qiskit import ClassicalRegister
+    from qiskit import execute
+    from CongX.extensions.standard.bmx import BMXGate
     import math
     # 量子レジスタ(グラフに対応した探索空間)を用意
     result_prepare_register = prepare_register(G, step)
     register_list = result_prepare_register[0]
     res_register = result_prepare_register[1] 
+    register_size = result_prepare_register[2] 
     
     # 量子レジスタ(コイン空間)を用意
     degree_set = adj_to_coin(G)
@@ -31,7 +34,7 @@ def QC4QWG(G, step):
     register_list.append(*coin_registers)
     
     # 観測結果を格納するための古典レジスタを用意
-    c = ClassicalRegister(数字、 'result')
+    cr = ClassicalRegister(register_size, 'result')
     
     # 量子回路を用意(レジスタをまとめる)
     qc =  QuantumCircuit(*register_list)
@@ -39,15 +42,11 @@ def QC4QWG(G, step):
     # 量子計算
     ## 初期状態を生成
     initial_distribution(G, qc, t0_register)
-    # 遷移の情報ごとにまとめてゲートとして呼び出す
-    
-    for step in steps:
-        for edge in edges:
-            コントロール1 遷移前
-            コントロール2 コイン
-            ターゲット1 
-        for gate in gates:
-        
+    # エッジを遷移の情報ごとにまとめてゲートとして呼び出す
+    gates = graph_to_gate(G, register_size, degree_set)
+    gates = sort_gate(gates)
+    for j in range(step):
+        call_gate(gates)
     # 観測
     qc.measure(res_register, cr)
     return qc
@@ -63,7 +62,7 @@ def prepare_register(G, step):
         qr = QuantumRegister(register_size, register_name)
         register_list.append(qr)
     res_register = register_list[-1]
-    return register_list, res_register  
+    return register_list, res_register, register_size  
     
 def adj_to_coin(G):
     # 隣接行列を元にコイン用のパラメータ(次数)をリストとして返す
@@ -84,15 +83,31 @@ def prepare_coin(degree_set):
         
     return coin_registers
 
-def edge_to_transition(edge, initial):
-    # 遷移先のノードへの移動に相当するゲートを作る。
-    
-    基本的には遷移先は初期化されているので反転するビットの情報を作る
-    Xゲートの対象を返す
-    
-    
-def
-
 def initial_distribution(G, qc, t0_register):
     # 重ね合わせで初期状態を準備
     qc.h(t0_register[0:-1])
+    
+
+def graph_to_gate(G, register_size, degree_set):
+    # エッジのリスト
+    edge_list = list(G.edges)
+    # エッジに対応した用いるコインのリスト
+    degrees = list(G.degree)
+    
+    
+    # エッジがノードにとって何番めのエッジか(どのコイン状態を用いるのか)
+    edge_number_list = [0,1,2,0,1,...みたいな]
+    # エッジごとにゲートに整形する
+    for edges in edge_list:
+        nodeA = edge[0]
+        nodeB = edge[1]
+        control = bin(int(nodeA))
+        ゼロパディング
+        
+    
+    
+def sort_gate():
+    # あとあとコンパイルしやすいようにターゲットゲートごとに同じものを揃える
+
+    
+def call_gate(edge, coin_info)
